cce175bb3b9d7bd8d4c13741523f9dfc03d06a5a
Neal Kaviratna <kaviratnaneal@gmail.com>
2022-01-20 12:56:01 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,32 +1,34 @@
 
-# External ID for the OtterTune role. Copy from OtterTune role setup wizard.
 variable "external_id" {
+  description = "External ID for the OtterTune role. Copy from OtterTune role setup wizard."
   type = string
 }
 
-# Role Name for the OtterTune role. This name can be whatever you like.```
 variable "iam_role_name" {
+  description = "Role Name for the OtterTune role. This name can be whatever you like."
   type    = string
   default = "OtterTuneRole"
 }
 
-# Pass in the parameter group ARNs that you'd like to allow OtterTune to optimize. 
-# Leave blank if you would like to run OtterTune in monitoring-only mode for now. This can be updated later.
-# ARN Format: arn:aws:rds:<region>:<account>:pg:<name>
 variable "tunable_parameter_group_arns" {
+  description = """ Pass in the parameter group ARNs that you'd like to allow OtterTune to optimize. 
+                    Leave blank if you would like to run OtterTune in monitoring-only mode for now. This can be updated later.
+                    ARN Format: arn:aws:rds:<region>:<account>:pg:<name>"""
   type    = list(string)
   default = []
 }
 
-# Pass in the aurora cluster parameter group ARNs that you'd like to allow OtterTune to optimize. 
-# Leave blank if you would like to run OtterTune in monitoring-only mode for now. This can be updated later.
-# ARN Format: arn:aws:rds:<region>:<account>:pg:<name>
+# 
 variable "tunable_aurora_cluster_parameter_group_arns" {
+  description = """ Pass in the aurora cluster parameter group ARNs that you'd like to allow OtterTune to optimize. 
+                    Leave blank if you would like to run OtterTune in monitoring-only mode for now. This can be updated later.
+                    ARN Format: arn:aws:rds:<region>:<account>:pg:<name>"""
   type    = list(string)
   default = []
 }
 
 variable "ottertune_account_id" {
+  description = "OtterTune Account ID, exposed to help terraform readability, no need to modify."
   type    = string
   default = "691523222388"
 }
